9c7f3ec2bb44a2594f39258bf2ae75c220335106
Regi-Web <regi-web@Regi-Webs-MacBook-Pro.local>
2024-03-17 17:07:39 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/amine.lahmary/",
+  base: "/Amine.Lahmary/",
   plugins: [
     vue(),
     VueDevTools(),
